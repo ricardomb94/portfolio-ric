bd74154a4c86b2c7b5c1cbce5c59b36477a9d02f
M'BOUKOU MONEKENE Aymard Ricardo <aymardmb@gmail.com>
2020-03-02 08:52:29 +0100

    Composant Projets intégré sans animation

---
+++ src/Fullstack/product.js
@@ -0,0 +1,28 @@
+const product = [
+    {
+        name:"Projet Zandoo",
+        img:"projetZandoo.png",
+    },
+    {
+        name:"Le petit commerce",
+        img:"petitCommerce.png",
+    },
+    {
+        name:"Projet Zandoo",
+        img:"projetZandoo.png",
+    },
+    {
+        name:"Le petit commerce",
+        img:"petitCommerce.png",
+    },
+    {
+        name:"Projet Zandoo",
+        img:"projetZandoo.png",
+    }, 
+    {
+        name:"Le petit commerce",
+        img:"petitCommerce.png",
+    }
+];
+
+export default product;
--- src/component/Projets.js
+++ src/component/Projets.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import product from '../Fullstack/product';
+import './Projets.css';
+
 
 
 class Projets extends Component {
     render() {
         return (
-            <div>
-                <h2>Projets</h2>
-            </div>
+            <section className="section3">
+                
+                <article className="project">
+                   
+                    {/*<div className="single">zandoo</div>
+                    <div className="single">vod</div>
+                    <div className="single">puissance 4</div>
+                    <div className="single">api</div>
+        <div className="single">mag</div>*/}
+                    {
+                        product.map((item,i)=>
+                            <div key={i} className="single">
+                                <img src={`media/${item.img}`} alt={item.name}className="pic"/>
+                            </div>
+                        ) 
+                    }    
+                </article>
+            </section>
         )
     }
 }

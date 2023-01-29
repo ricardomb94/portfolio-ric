7e59987d44110715f7f6cca95d687ff98f7cd97b
ricardomb94 <aymardmb@gmail.com>
2023-01-29 06:17:55 +0100

    Updates

---
+++ precache-manifest.c406981b550eb182465cd6d640e53c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ff20409a55b665c8fd5d0c7ff8c074",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b2336b9ab6b0c90f81f",
+    "url": "/static/css/main.d29e18af.chunk.css"
+  },
+  {
+    "revision": "1779898c2b857a969020",
+    "url": "/static/js/2.8706e6d0.chunk.js"
+  },
+  {
+    "revision": "41149867de3c61e0badbcaf340cac4fd",
+    "url": "/static/js/2.8706e6d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b2336b9ab6b0c90f81f",
+    "url": "/static/js/main.105b40fc.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
